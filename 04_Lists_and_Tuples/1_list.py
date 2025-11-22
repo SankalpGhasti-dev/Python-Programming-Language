6d5aca97868e2df2279b4a39471eb2ee0ab9c86f
Sankalp <ghastisankalp7@gmail.com>
2025-11-23 03:26:43 +0530

    updated new codes

--- 04_Lists_and_Tuples/1_list.py
+++ 04_Lists_and_Tuples/1_list.py
@@ -15,19 +15,3 @@ print(friends[0:4])
 friends.append("Yash")
 print(friends)
 
-l1 = [ 23, 4, 64, 67, 27]
-# l1.sort()
-# print(l1)
-
-# l1.reverse()
-# print(l1)
-
-# l1.insert(4,22222)
-# insert 22222 such that it's index in the list is 4
-# print(l1)
-
-# l1.pop(2)
-# print(l1)
-
-l1.remove(64)
-print(l1)
