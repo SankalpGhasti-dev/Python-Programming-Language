46a77b114be3fe51018300a56a74238701c35dbe
Sankalp <ghastisankalp7@gmail.com>
2025-12-09 12:06:25 +0530

    updated new codes

---
+++ 05_Dictionary_Sets/1_dict.py
@@ -0,0 +1,9 @@
+
+marks = {
+    "Harry" : 100, 
+    "Rohan" : 25, 
+    "Skill" : 70,
+}
+
+print(marks, type(marks))
+print(marks["Harry"])
---
+++ 05_Dictionary_Sets/2_dict_methods.py
@@ -0,0 +1,24 @@
+
+marks = {
+    "Harry" : 100, 
+    "Rohan" : 25, 
+    "Skill" : 70,
+}
+
+# The left one (Harry, Rohan ) are keys and the right one (100, 25) are values.
+
+print(marks.items())
+
+print(marks.keys())
+
+print(marks.values())
+
+marks.update({"Harry": 98, "Renuka": 100})
+print(marks)
+
+# If the key is already present then its value will be updated else new key:value pair will be added.
+
+# Dictionary are mutable.
+
+print(marks.get("Harry2")) # Prints None
+# print(marks["Harry2"])     # Returns an error
