d2ed5eb168e1a6ed242a5eee017496202c5ad45f
Sankalp <ghastisankalp7@gmail.com>
2026-01-26 22:46:37 +0530

    updated new codes

--- 09_File_I_O_/05_with.py
+++ 09_File_I_O_/05_with.py
@@ -4,7 +4,7 @@
 
 # The same can be written using with statement like this 
 
-with open("09_File_I_O_/file.txt") as f:
-    print(f.read())
+with open("09_File_I_O_/file.txt") as txt:
+    print(txt.read())
 
 # You don't have to explicitly close the file when using with statement. It automatically takes care of closing the file after the nested block of code.
--- 09_xPractice_set/03_Problem3.py
+++ 09_xPractice_set/03_Problem3.py
@@ -1,7 +1,14 @@
 
-def generateTable(i):
-    print(f"{2} X {i} = {2 * i}")
+def generateTable(n):
+    Table = ""
+    for i in range(1,11):
+        print(f"{n} X {i} = {n* i}")
+    
+    with open(f"Table/table_of_{n}.txt", "w") as f:
+        f.write(Table)
+        
 
 
 for i in range(2, 21):
     generateTable(i)
+   
